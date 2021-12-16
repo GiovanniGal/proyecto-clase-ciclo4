1d9c12a176d6d4bce39d05412f1c8e85efaca096
Giovanni Galindo <giovanni09@gmail.com>
2021-12-15 22:25:57 -0500

    ajustes en avances consultas y creacion

--- models/avance/resolvers.ts
+++ models/avance/resolvers.ts
@@ -6,7 +6,13 @@ const resolversAvance = {
       const avances = await AdvancementModel.find().populate('proyecto')
       .populate('creadoPor');;
       return avances;
-    }
+    },
+    filtrarAvance: async (parent, args) => {
+      const avanceFiltrado = await AdvancementModel.find({proyecto:args.idProyecto})
+        .populate('proyecto')
+        .populate('creadoPor');
+      return avanceFiltrado;
+    },
   },
   Mutation:{
     crearAvance: async (parent, args) => {
@@ -17,7 +23,8 @@ const resolversAvance = {
         creadoPor: args.creadoPor,
       });
       return avanceCreado;   
-    }
+    },
+
 
   },
 
